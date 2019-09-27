d497dc7ce19b6b999dd69b9db73190b1a65b8911
Jason Gao <jasongzj@163.com>
2019-09-27 14:30:29 +0800

    优化配置白名单逻辑

--- app/Http/Controllers/Admin/RaffleWhitelistController.php
+++ app/Http/Controllers/Admin/RaffleWhitelistController.php
@@ -19,9 +19,19 @@ class RaffleWhitelistController extends Controller
     {
         $raffleId = $request->input('raffle_id');
         $awardId = $request->input('award_id');
+        $userIds = array_filter($request->input('user_ids'));
+
+        // 查询是否已配置在其他奖项白名单中
+        $userExist = RaffleWhitelist::query()->where('raffle_id', $raffleId)
+            ->whereIn('user_id', $userIds)
+            ->exists();
+        if ($userExist) {
+            return $this->failed('用户已配置于其他奖项中', 400);
+        }
+
         $time = Carbon::now();
         $whitelist = [];
-        foreach ($request->input('user_ids') as $userId) {
+        foreach ($userIds as $userId) {
             $whitelist[] = [
                 'raffle_id' => $raffleId,
                 'award_id' => $awardId,
@@ -30,8 +40,8 @@ class RaffleWhitelistController extends Controller
                 'updated_at' => $time,
             ];
         }
-        $result = DB::transaction(function () use ($whitelist) {
-            RaffleWhitelist::query()->delete();
+        $result = DB::transaction(function () use ($whitelist, $awardId) {
+            RaffleWhitelist::query()->where('award_id', $awardId)->delete();
             $result = DB::table('raffle_whitelist')->insert($whitelist);
             return $result;
         });

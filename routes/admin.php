00124b659b8a850dda3888e853d600fb16da3be2
Jason Gao <jasongzj@163.com>
2019-09-27 15:08:57 +0800

    退出登录请求方法更改

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'AuthController@login');
 
 Route::group(['middleware' => 'auth:admin'], function () {
-    Route::get('logout', 'AuthController@logout');
+    Route::post('logout', 'AuthController@logout');
 
     Route::get('users/search', 'UsersController@search'); // 搜索用户
 

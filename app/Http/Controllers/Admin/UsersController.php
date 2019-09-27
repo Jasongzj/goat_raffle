79bee064b1c4973350d03c5f78343e95bad169d8
Jason Gao <jasongzj@163.com>
2019-09-27 14:33:58 +0800

    优化搜索逻辑

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\RaffleWhitelist;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -13,10 +14,22 @@ class UsersController extends Controller
         if (!$request->input('name')) {
             return $this->success([]);
         }
-        $list = User::query()
+
+        $query = User::query();
+
+        if ($raffleId = $request->input('raffle_id')) {
+            $whitelistUserIds = RaffleWhitelist::query()->where('raffle_id', $raffleId)
+                ->get(['user_id'])
+                ->pluck('user_id')
+                ->all();
+            $query = $query->whereNotIn('id', $whitelistUserIds);
+        }
+
+        $list = $query
             ->where('nick_name', 'like', '%' . $request->input('name') . '%')
             ->select(['id', 'nick_name', 'avatar_url'])
             ->get();
+        
         return $this->success($list);
     }
 }
